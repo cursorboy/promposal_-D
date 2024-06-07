659c63a96145148b6a7e4465682d1055369e435f
Piam Parekh <78398731+cursorboy@users.noreply.github.com>
2024-06-06 22:26:22 -0700

    Update Poem.svelte

--- src/Poem.svelte
+++ src/Poem.svelte
@@ -17,17 +17,19 @@
     title="To {name}"
     border
   >
-    <p>I spent too much time coding this</p>
-    <p>But there's one thing I cannot miss</p>
-    <p>And it's taking you to the prom</p>
-    <p>and im so glad for our comms</p>
-    <p>Oh god, im a nerd</p>
-    <p>but i know thats what u wouldve preferred</p>
-    <p>thank you for saying yes</p>
-    <p> now im excited to see ur dress</p>
-    <p>lmk if this was a success</p>
-    <p>so now u won't be stressed</p>
-    <a href="https://promposal-rosy.vercel.app">click here for more</a>
+    <p>I always wondered what hurt the most</p>
+    <p>whether it was seeing you move on or the ways you talked about me</p>
+    <p>I think it was, after our 2 years, you showed nothing but resentment</p>
+    <p>Since the brekaup, I heard so much about you. the shitty things you said about me </p>
+    <p>all those reposts I used to once stalk, the things you told others about me, some true, some untrue</p>
+    <p>I am not trying to play victim, i just had some frustrations, I know i was far from perfect, but tbf I had loved you to the ends of the earth</p>
+    <p>I NEVER meant to hurt you so deeply if I did</p>
+    <p>From the bottom of my heart, I am sorry</p>
+    <p>the lies, everything</p>
+    <p>I am not hear to pick fights or anything</p>
+    <p>If money was the issue, I never meant to make you feel like I conned you out of money</p>
+    <p>So if you're reading this whether it be a fluke or something itched in the back of your brain</p>
+    <p>I will go ahead and pay you back for the switch, just contact me saying you read this message</p>
   </Article>
 </div>
 
